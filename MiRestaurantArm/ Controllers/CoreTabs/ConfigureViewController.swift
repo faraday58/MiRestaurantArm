d1af2584b81ee6c8dfd8367078552ce88d4b0d22
Armando <jarodriguezvera@gmail.com>
2023-08-23 16:10:03 -0600

    Add ViewController, NavigationIterm, StoryBoards, Push navigation

---
+++ MiRestaurantArm/ Controllers/CoreTabs/ConfigureViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ConfigureViewController.swift
+//  MiRestaurantArm
+//
+//  Created by Armando Rodríguez on 22/08/23.
+//
+
+import UIKit
+
+class ConfigureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ MiRestaurantArm/ Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MiRestaurantArm
+//
+//  Created by Armando Rodríguez on 21/08/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+        let uiImage = UIImage(systemName: "person.fill")
+        
+        self.navigationController?.title = "Home"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: uiImage, style: .plain, target: self, action: #selector(didTapButton))
+    }
+
+    @objc private func didTapButton()
+    {
+        let registerStoryBoard = UIStoryboard(name: "RegisterStoryBoard", bundle: .main)
+        let registerViewControler = registerStoryBoard.instantiateViewController(withIdentifier: "rgisterSB") as! RegisterViewController
+        self.navigationController?.pushViewController(registerViewControler, animated: true)
+    }
+
+}
+
