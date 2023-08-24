e16a13969f028c187a65731f046db7de55236561
Armando <jarodriguezvera@gmail.com>
2023-08-24 12:47:05 -0600

    Add constraints to image

--- MiRestaurantArm/ Controllers/Onboarding/RegisterViewController.swift
+++ MiRestaurantArm/ Controllers/Onboarding/RegisterViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 class RegisterViewController: UIViewController {
     @IBOutlet weak var logoImageView: UIImageView!
     
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var retPasswordTextField: UITextField!
+    
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,8 +37,26 @@ class RegisterViewController: UIViewController {
         logoImageView.image = UIImage(named: "perfil")
         logoImageView.contentMode = .scaleAspectFill
         logoImageView.layer.cornerRadius = logoImageView.bounds.width / 2
+        errorLabel.isHidden = false
+        errorLabel.text = "chale"
+        
+    }
+    
+    
+    @IBAction func registerAction(_ sender: Any) {
+        let user = userTextField.text
+        let password = passwordTextField.text
+        let retPassword =  retPasswordTextField.text
+        
+        if password != retPassword {
+            errorLabel.isHidden = false
+            errorLabel.text = "Password don't match"
+        }else
+        {
+            errorLabel.isHidden = true
+        }
         
         
     }
-
+    
 }
